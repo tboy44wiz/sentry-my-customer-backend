76b0867f8521fe365d1665287e962b4fe77b6c63
Micah Elijah <melijah200@gmail.com>
2020-06-19 13:35:27 +0100

    updated database schemas

--- models/debt_reminders.js
+++ models/debt_reminders.js
@@ -4,7 +4,7 @@ const DebtReminder = new mongoose.Schema({
   ts_ref_id: { type: Schema.Types.ObjectId, ref: 'transaction' },
   message: { type: String, required: true },
   status: { type: String, required: true },
-  expected_pay_date: { type: Date, default: Date.now }
+  expected_pay_date: { type: Date, default: Date.now() }
 }, { timestamps: true })
 
 module.exports = mongoose.model('debt_reminder', DebtReminder)
--- models/transaction_role.js
+++
@@ -1,7 +0,0 @@
-const mongoose  = require("mongoose");
-
-const transactionRoleSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    value: { type: String, required: true},
-    ref_transaction_role: { type: String, required: true }
-})
