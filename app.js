85f1c8380e1d2d1056261ff8c1e086b7dfc7b2b6
Tolulope Folorunso <tolufolorunso@yahoo.com>
2020-06-20 03:48:51 +0100

    Store crud implemented

--- app.js
+++ app.js
@@ -1,46 +1,51 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-require('dotenv').config()
+require("dotenv").config();
 
 var url = process.env.MONGOLAB_URI;
 
 const app = express();
 
 const ejs = require("ejs");
-var cors = require('cors');
-const documentation = require('./routes/documentation')
-const phone_verification = require('./routes/verify-phone-number')
-const example = require('./routes/example');
-const phone_call_api = require('./controllers/phone_call_api');
-require('./routes/transactions.js')(app);
-const mongoose = require('mongoose');
+var cors = require("cors");
+const documentation = require("./routes/documentation");
+const phone_verification = require("./routes/verify-phone-number");
+const example = require("./routes/example");
+const store = require("./routes/stores");
+const phone_call_api = require("./controllers/phone_call_api");
+require("./routes/transactions.js")(app);
+const mongoose = require("mongoose");
 app.use(cors());
 
 mongoose.Promise = global.Promise;
 
 // Connecting to the database
-mongoose.connect(url, {
-	useNewUrlParser: true
-}).then(() => {
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
     console.log("Successfully connected to the database");
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Exiting now...", err);
     process.exit();
-});
+  });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.set("view engine", "ejs");
 
-app.use(documentation)
-app.use(phone_verification)
-app.use(example)
+app.use(documentation);
+app.use(phone_verification);
+app.use(example);
+app.use(store);
 
 /**
  * phone call api route below
@@ -48,9 +53,8 @@ app.use(example)
  * A post request should  be made to localhost:5000/api/v1/call
  *
  */
-app.use('/api', phone_call_api);
+app.use("/api", phone_call_api);
 
 app.listen(5000, () => {
-    console.log(`app running on port: http://localhost:5000`);
+  console.log(`app running on port: http://localhost:5000`);
 });
-
