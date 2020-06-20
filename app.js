bd2eb170b0138e0b486fd288cd39ff53e4f8549b
Ambrose <ekponoambrose@gmail.com>
2020-06-20 06:17:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,29 +2,24 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
-require('dotenv').config();
-
-require('./routes/transactions.js');
+require('dotenv').config()
 var url = process.env.MONGOLAB_URI;
-//var url = 'mongodb://localhost/MyCustomer';
-
 const app = express();
 
 const ejs = require("ejs");
 var cors = require('cors');
-
-const register = require('./routes/register_route');
-const login = require('./routes/login_route');
 const documentation = require('./routes/documentation')
+const customer = require('./routes/customer')
 const phone_verification = require('./routes/verify-phone-number')
 const example = require('./routes/example');
 const phone_call_api = require('./controllers/phone_call_api');
+
 const messagingAPI = require("./routes/messaging");
-
-require('./routes/transactions.js')(app);
 const mongoose = require('mongoose');
-
+const transactions = require('./routes/transactions');
+const store = require('./routes/stores.js');
+// const register = require('./routes/register_route');
+// const login = require('./routes/login_route');
 app.use(cors());
 
 mongoose.Promise = global.Promise;
@@ -48,18 +43,20 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 app.set("view engine", "ejs");
 
 app.use(documentation)
+app.use(customer)
 app.use(phone_verification)
 app.use(messagingAPI)
 app.use(example)
-
+app.use(transactions);
+app.use(store);
+// app.use(register);
+// app.use(login);
 /**
  * phone call api route below
  *
  * A post request should  be made to localhost:5000/api/v1/call
  *
  */
-app.use('/register', register);
-app.use('/login', login);
 app.use('/api', phone_call_api);
 
 const port = 5000;
@@ -67,3 +64,4 @@ app.listen(port, () => {
     console.log(`app running on port: ` + port);
 });
 
+require('dotenv').config()
