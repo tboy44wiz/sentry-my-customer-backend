5d3267304a19a552531f8e63444857c8b12b21a6
Arturo J. Delgado S <ajdelgados@gmail.com>
2020-06-19 22:08:32 -0400

    Adde /customer/:customerId, /customer/update/:customerId and /customer/delete/:customerId

--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -1,4 +1,3 @@
-
 const Customer = require("../models/customer");
 
 exports.create = async (req, res) => {// Validate request
@@ -24,6 +23,95 @@ exports.create = async (req, res) => {// Validate request
             phone: customer.phone_number
         }
     });
+}
+
+exports.getById = (req, res) => {
+    try {
+        Customer.findById(req.params.customerId, (error, customer) => {
+            if(error) {
+                res.status(404).send({
+                    status: "fail",
+                    message: error.message
+                });
+            } else {
+                res.status(200).json({
+                    status: "success",
+                    data: {
+                        id: customer._id,
+                        name: customer.name,
+                        phone: customer.phone_number
+                    }
+                })
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "fail",
+            message: error.message,
+        });
+    }
+}
+
+exports.updateById = (req, res) => {
+    try {
+        Customer.findById(req.params.customerId, (error, customer) => {
+            if(error) {
+                res.status(404).send({
+                    status: "fail",
+                    message: error.message
+                });
+            } else {
+                customer.name = req.body.name
+                customer.phone_number = req.body.phone
+                customer.save()
+                res.status(200).json({
+                    status: "success",
+                    data: {
+                        id: customer._id,
+                        name: customer.name,
+                        phone: customer.phone_number
+                    }
+                })
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "fail",
+            message: error.message,
+        });
+    }
+}
+
+exports.deleteById = (req, res) => {
+    try {
+        Customer.findByIdAndDelete(req.params.customerId, (error, customer) => {
+            if(error) {
+                res.status(404).json({
+                    status: "fail",
+                    //message: error.message,
+                });
+            } else if(!customer) {
+                res.status(404).json({
+                    status: "fail",
+                    message: "Not found",
+                });
+            } else {
+                res.status(200).json({
+                    status: "success",
+                    data: {
+                        id: customer._id,
+                        name: customer.name,
+                        phone: customer.phone_number
+                    }
+                })
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "fail",
+            message: error.message,
+        });
+    }
 }
 
 exports.getAll = async (req, res) => {
--- routes/customer.js
+++ routes/customer.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const customerController = require("./../controllers/customer.controller.js");
 
 router.post('/customer/new', customerController.create)
-//router.get('/customer/:customerId', customerController.getById)
-//router.put('/customer/update/:customerId', customerController.updateById)
-//router.delete('/customer/delete/:customerId', customerController.deleteById)
 router.get('/customer/all', customerController.getAll)
+router.put('/customer/update/:customerId', customerController.updateById)
+router.delete('/customer/delete/:customerId', customerController.deleteById)
+router.get('/customer/:customerId', customerController.getById)
 
 module.exports = router
