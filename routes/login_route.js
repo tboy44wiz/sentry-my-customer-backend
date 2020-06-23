e22db6939c20e5e934d94c468ee52d32037b0923
Philip <55332143+nerdyphil@users.noreply.github.com>
2020-06-23 10:30:14 +0100

    Update login_route.js

--- routes/login_route.js
+++ routes/login_route.js
@@ -4,7 +4,7 @@ const LoginController = require('../controllers/login_controler');
 const router = express.Router();
 
 //User registration route
-router.post('/user/login', LoginController.loginUser);
+router.post('/user', LoginController.loginUser);
 
 //Customer registration route
 router.post('/customer', LoginController.loginCustomer);
