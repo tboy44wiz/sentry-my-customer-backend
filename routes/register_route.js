0c304e5279f28e95e05e13e06d344b7e9eba91cf
Philip <55332143+nerdyphil@users.noreply.github.com>
2020-06-24 10:55:35 +0100

    Update register_route.js

--- routes/register_route.js
+++ routes/register_route.js
@@ -4,9 +4,9 @@ const RegisterController = require('../controllers/register_controler');
 const router = express.Router();
 
 //User registration route
-router.post('/user', RegisterController.registerUser);
+router.post('/register/user', RegisterController.registerUser);
 
 //Customer registration route
-router.post('/customer', RegisterController.registerCustomer);
+router.post('/register/customer', RegisterController.registerCustomer);
 
 module.exports = router;
