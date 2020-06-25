612ec92c87c7f51b86775dc4c0d1d1a6dc74bc6f
Emmanuel Menyaga <sparklinsparky@gmail.com>
2020-06-25 10:53:09 +0100

    fixed collection.ensureIndex is deprecated mongoose deprecation warning

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config()
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 const { MONGOLAB_URI, API_PORT } = process.env;
 const app = express();
 
 const ejs = require("ejs");
-var cors = require('cors');
-const documentation = require('./routes/documentation')
-const customer = require('./routes/customer')
-const phone_verification = require('./routes/verify-phone-number')
-const example = require('./routes/example');
-const phone_call_api = require('./controllers/phone_call_api');
-const user= require('./routes/user');
-
+var cors = require("cors");
+const documentation = require("./routes/documentation");
+const customer = require("./routes/customer");
+const phone_verification = require("./routes/verify-phone-number");
+const example = require("./routes/example");
+const phone_call_api = require("./controllers/phone_call_api");
+const user = require("./routes/user");
 
 const messagingAPI = require("./routes/messaging");
-const mongoose = require('mongoose');
-const transactions = require('./routes/transaction');
-const store = require('./routes/stores.js');
-const register = require('./routes/register_route');
-const login = require('./routes/login_route');
+const mongoose = require("mongoose");
+const transactions = require("./routes/transaction");
+const store = require("./routes/stores.js");
+const register = require("./routes/register_route");
+const login = require("./routes/login_route");
 const emailAPI = require("./routes/sendMail");
 const complainRouter = require("./routes/complaint");
 const errorPage = require("./routes/error-page");
@@ -30,20 +29,24 @@ app.use(cors());
 mongoose.Promise = global.Promise;
 
 // Connecting to the database
-mongoose.connect(MONGOLAB_URI, {
+mongoose
+  .connect(MONGOLAB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
     console.log("Successfully connected to the database");
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Exiting now...", err);
     process.exit();
-});
+  });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.set("view engine", "ejs");
 
@@ -54,7 +57,7 @@ app.use(messagingAPI);
 app.use(emailAPI);
 app.use(transactions);
 app.use(store);
-app.use("/register",register);
+app.use("/register", register);
 app.use(login);
 app.use(complainRouter);
 app.use(user);
@@ -70,7 +73,7 @@ app.use("/", phone_call_api);
 app.use(errorPage);
 const port = API_PORT || 5000;
 app.listen(port, () => {
-    console.log(`app running on port: ` + port);
+  console.log(`app running on port: ` + port);
 });
 
-require('dotenv').config()
+require("dotenv").config();
