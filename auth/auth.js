77f9717f7b00154850fa0620128d159c4f0c5d56
Dev Body <dasther@outlook.com>
2020-06-26 03:52:43 -0400

    MISSING FIELDS ON THE COMPLAINT API #77 RESOLVED

--- auth/auth.js
+++ auth/auth.js
@@ -1,17 +1,19 @@
 var jwt = require('jsonwebtoken');
 var config = process.env;
 
-function verifyToken(req, res, next) {
+const verifyToken = (req, res, next) => {
   var token = req.body.token || req.query.token || req.headers['x-access-token'];
-  if (!token)
+
+  if (!token) {
     return res.status(403).send({ auth: false, message: 'No token provided.' });
+  }
     
   jwt.verify(token, config.JWT_KEY, function(err, decoded) {
     if (err){
-		return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-	};
+		  return res.status(401).send({ auth: false, message: 'Failed to authenticate token.' });
+	  };
     req.user = decoded;
-	//console.log(req.user);
+    // console.log(req.user);
     next();
   });
 }
