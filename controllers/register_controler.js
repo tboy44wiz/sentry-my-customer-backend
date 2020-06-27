a80f9f8c93670f8218ac7d9571922d7e5554d25a
Micah Elijah <melijah200@gmail.com>
2020-06-27 11:56:58 +0100

    update from develop

--- controllers/register_controler.js
+++ controllers/register_controler.js
@@ -44,7 +44,6 @@ module.exports.registerUser = async (req, res, next) => {
         last_name: value.last_name,
         email: value.email,
         password: value.password,
-        api_token: token,
         api_token: token
     });
 
