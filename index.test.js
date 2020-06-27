7936c55ab1bca7d7712be9cf6e4b454d4a61bed0
Arturo J. Delgado S <ajdelgados@gmail.com>
2020-06-27 17:26:36 -0400

    Added unit test for customer

--- index.test.js
+++ index.test.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const { MONGOLAB_URI, API_PORT } = process.env;
 
 describe('All app', () => {
+  let api_token;
+  let customer_id;
 
   /*beforeAll(async () => {
     await mongoose.connect(MONGOLAB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -35,12 +37,125 @@ describe('All app', () => {
           first_name: expect.any(String),
           last_name: expect.any(String),
           email: expect.any(String),
-          is_active: false,
+          is_active: expect.any(Boolean),
           password: expect.any(String),
           user_role: expect.any(String)
         }),
       }),
     );
+
+    api_token = res.data.api_token
+
+    done()
+  });
+
+  test('customer new', async done => {
+    let res = await axios.post(`http://localhost:${API_PORT}/customer/new`, {
+      "name": "namet",
+      "phone": "888888"
+    });
+
+    expect(res.data).toEqual(
+      expect.objectContaining({
+        status: "success",
+        data: expect.objectContaining({
+          id: expect.any(String),
+          name: "namet",
+          phone: 888888
+        }),
+      }),
+    );
+
+    customer_id = res.data.data.id
+
+    done()
+  });
+
+  test('customer getById', async done => {
+    let res = await axios.get(`http://localhost:${API_PORT}/customer/${customer_id}`, {
+      headers: {"x-access-token": api_token}
+    })
+
+    expect(res.data).toEqual(
+      expect.objectContaining({
+        status: "success",
+        data: expect.objectContaining({
+          id: customer_id,
+          name: "namet",
+          phone: 888888
+        }),
+      }),
+    );
+
+    done()
+  });
+
+  test('customer update', async done => {
+    let res = await axios.put(`http://localhost:${API_PORT}/customer/update/${customer_id}`, {
+      "name": "namet",
+      "phone": "999"
+    }, {
+      headers: {"x-access-token": api_token}
+    })
+
+    expect(res.data).toEqual(
+      expect.objectContaining({
+        status: "success",
+        data: expect.objectContaining({
+          id: customer_id,
+          name: "namet",
+          phone: "999"
+        }),
+      }),
+    );
+
+    done()
+  });
+
+  test('customer delete', async done => {
+    let res = await axios.delete(`http://localhost:${API_PORT}/customer/delete/${customer_id}`, {
+      headers: {"x-access-token": api_token}
+    })
+
+    expect(res.data).toEqual(
+      expect.objectContaining({
+        status: "success",
+        data: expect.objectContaining({
+          id: customer_id,
+          name: "namet",
+          phone: 999
+        }),
+      }),
+    );
+
+    done()
+  });
+
+  test('customer all', async done => {
+    let res = await axios.get(`http://localhost:${API_PORT}/customer/all`, {
+      headers: {"x-access-token": api_token}
+    })
+
+    expect(res.data).toEqual(
+      expect.objectContaining({
+        status: "success",
+        result: res.data.data.length,
+        data: expect.any(Array)
+        /*expect.toContain(),*/
+      }),
+    );
+
+    res.data.data.forEach(element => {
+      expect(element).toEqual(
+        expect.objectContaining({
+          _id: expect.any(String),
+          name: expect.any(String),
+          phone_number: expect.any(Number),
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String)
+        })
+      )
+    });
 
     done()
   });
