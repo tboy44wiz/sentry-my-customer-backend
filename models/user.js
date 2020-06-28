805c4c8ed0526b70f452a0a5caf2a5b963d44c23
Judge Godwins <judgegodwins@gmail.com>
2020-06-28 13:58:07 +0100

    fix debt/all should return debt for a particular user only

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 
 const User = new mongoose.Schema({
-  phone_number: { type: Number, required: true,  unique: true }, 
+  phone_number: { type: String, required: true,  unique: true }, 
   first_name: { type: String, required: true }, 
   last_name: { type: String, required: true }, 
   email: { type: String, required: true,  unique: true }, 
