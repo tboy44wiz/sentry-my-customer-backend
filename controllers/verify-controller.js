4cedbd67eb81db1c9f7b0ffbffa8060b367bf7e2
chinazaaa <50022245+chinazaaa@users.noreply.github.com>
2020-07-03 08:12:54 +0100

    Revert "Update user status to true upon verification"

--- controllers/verify-controller.js
+++ controllers/verify-controller.js
@@ -1,70 +1,67 @@
-const Response = require("../util/response_manager");
-const HttpStatus = require("../util/http_status");
-const express = require("express");
+const Response = require('../util/response_manager');
+const HttpStatus = require('../util/http_status');
+const express = require('express');
 const User = require("../models/user");
 
 const router = express.Router();
 
-router.use(require("body-parser").urlencoded({ extended: true }));
-const messagebird = require("messagebird")("S2LorOevmKnSHrcS9KBXnVXyq"); //4505 test
+router.use(require("body-parser").urlencoded({extended: true}));
+const messagebird = require('messagebird')('S2LorOevmKnSHrcS9KBXnVXyq'); //4505 test
 // const messagebird = require('messagebird')('jXFk6Krvh8Lw24Xn1Kw0XjX4M');//production
 
 module.exports = {
-  //NOTE: both keys can only work with +2348145502505
-  initialverification(req, res) {
-    var params = {
-      originator: "MyCustomer",
-      template: "Your MyCustomer verification code is: %token",
-    };
-    messagebird.verify.create(req.body.phone, params, function (err, response) {
-      if (err) {
-        res.status(401).json({
-          status: "Fail",
-          message: err,
-        });
-      } else {
-        //Now we render a page to enter token
-        //There should be a hidden input which will contain the id of the process
-        res.status(200).json({
-          status: "success",
-          response: response,
-        });
-      }
-    });
-  },
-  verifyPhone(req, res) {
-    try {
-      const id = req.body.id;
-      const token = req.body.token;
-      const phone = req.body.phone;
-      messagebird.verify.verify(id, token, (err, response) => {
-        if (err) {
-          res.status(err.statusCode).json({
-            success: false,
-            message: "Phone number could not be verified",
-            error: err,
-          });
-        } else {
-          User.findOne({ phone_number: phone })
-            .then((success) => {
-              success.is_active = true;
-              return success.save();
+    //NOTE: both keys can only work with +2348145502505
+    initialverification(req, res) {
+        var params = {
+            originator: "MyCustomer",
+            template: "Your MyCustomer verification code is: %token"
+        };
+        messagebird.verify.create(req.body.phone, params, function (err, response) {
+            if (err) {
+                res.status(401).json({
+                    status: "Fail",
+                    message: err
+                })
+            }else{
+            //Now we render a page to enter token
+            //There should be a hidden input which will contain the id of the process
+            res.status(200).json({
+                status: "success",
+                response: response
             })
-            .then((activeUser) =>
-              res.status(200).json({
-                success: true,
-                message: "account activated",
-                data: activeUser,
-              })
-            )
-            .catch((err) => {
-              res.status(404).json({
-                success: false,
-                message: "Could not find user with that phone number",
-              });
-            });
         }
-      });
-    } catch (e) {}
-  },
+        });
+    },
+     verifyPhone(req, res) {
+         try {
+             const id = req.body.id
+             const token = req.body.token
+             const phone = req.body.phone
+             messagebird.verify.verify(id, token, (err, response) => {
+                 if (err) {
+                     res.status(err.statusCode).json({
+                         success: false,
+                         message: "Phone number could not be verified",
+                         error: err
+                     });
+                 } else {
+                    User.findOne({ phone_number:  phone}).catch(err =>{
+                        res.status(404).json({
+                            success: false,
+                            message: "Could not find user with that phone number"
+                        })
+                    }).then(success =>{
+                        success.is_active = 1
+                        success.save()
+                    })
+                     res.status(200).json({
+                         success: "true",
+                         message: response
+                     })
+                 }
+             })
+         } catch (e) {
+
+         }
+     }
 };
