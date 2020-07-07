3f4fcf47501c68745e3b210eb51f30fec3bdb1ec
Emmanuel Menyaga <sparklinsparky@gmail.com>
2020-07-07 23:34:41 +0100

    feat: Mail service up and running

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const emailController = require("./../controllers/emailController");
-const auth = require('../auth/auth');
+const auth = require("../auth/auth");
 
-router.post('/reminder/email/:customer_id', auth, auth,emailController.sendMail);
+router.post("/reminder/email/:customer_id", auth, emailController.sendMail());
 
-module.exports = router
+module.exports = router;
