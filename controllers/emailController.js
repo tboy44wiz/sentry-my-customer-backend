f1ac0337d5a5645b659e9dcb3d7963844b27e79b
Emmanuel Menyaga <sparklinsparky@gmail.com>
2020-07-08 03:28:48 +0100

    mail service

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -6,6 +6,9 @@ module.exports = {
     try {
       const transporter = nodemailer.createTransport({
         service: process.env.MAIL_SERVICE,
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT,
+        secure: false,
         auth: {
           user: process.env.MAIL_USER,
           pass: process.env.MAIL_PASSWORD,
