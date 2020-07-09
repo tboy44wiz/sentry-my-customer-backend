79aef61b9a62216cd1e241b60e97715a9517c09e
Emmanuel Menyaga <sparklinsparky@gmail.com>
2020-07-09 03:02:02 +0100

    feat: password update

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const emailController = require("./../controllers/emailController");
 const auth = require("../auth/auth");
+const pUpdate = require("../controllers/passwordupdate");
 
 router.post("/reminder/email/:customer_id", auth, emailController.sendMail());
+router.post("/update-password", auth, pUpdate());
 
 module.exports = router;
