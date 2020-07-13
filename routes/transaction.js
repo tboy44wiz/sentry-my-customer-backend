2cc19cd3c24cd64d399d152aea83f07144229615
Arturo J. Delgado S <ajdelgados@gmail.com>
2020-07-13 16:00:14 -0400

    Modified transaction controller

--- routes/transaction.js
+++ routes/transaction.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const transactions = require("../controllers/transaction.controller.js");
+const bodyValidator = require("../util/body_validator");
 const auth = require("../auth/auth");
 
 // Create a new Transaction
-router.post("/transaction/new", auth, transactions.create);
+router.post("/transaction/new", auth, transactions.validate("create"), bodyValidator, transactions.create);
 
 // Retrieve all Transactions
-router.get("/transaction", auth, transactions.findAll);
+router.get("/transaction", auth, transactions.validate("find"), bodyValidator, transactions.findAll);
 
 // Retrieve a single Transaction with transaction_id
-router.get("/transaction/:transaction_id", auth, transactions.findOne);
+router.get("/transaction/:transaction_id", auth, transactions.validate("find"), bodyValidator, transactions.findOne);
 
 // Update a Transaction with transaction_id
-router.put("/transaction/update/:transaction_id", auth, transactions.update);
+router.patch("/transaction/update/:transaction_id", auth, transactions.validate("update"), bodyValidator, transactions.update);
 
 // Delete a Transaction with transaction_id
-router.delete("/transaction/delete/:transaction_id", auth, transactions.delete);
+router.delete("/transaction/delete/:transaction_id", auth, transactions.validate("find"), bodyValidator, transactions.delete);
 
 module.exports = router;
