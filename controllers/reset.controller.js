d7ad190ff1b584f38fb4265c788fb2fef0626fc5
Judge Godwins <judgegodwins@gmail.com>
2020-07-13 11:27:46 +0100

    add route update/password

--- controllers/reset.controller.js
+++ controllers/reset.controller.js
@@ -1,4 +1,5 @@
 const User = require("../models/store_admin");
+const { body } = require('express-validator/check');
 const bCrypt = require("bcryptjs");
 const africastalking = require("africastalking")({
   apiKey: process.env.AFRICASTALKING_API_KEY,
@@ -8,12 +9,25 @@ const africastalking = require("africastalking")({
 const makeid = require("../util/code_random");
 const codeLength = 6;
 
+exports.validate = (method) => {
+  switch(method) {
+    case 'body':
+      return [
+        body('old_password').isString(),
+        body('new_password').isString().isLength({min: 6}).withMessage("Password must be 6 characters long")
+      ];
+
+  }
+}
+
+
 module.exports.recover = async (req, res) => {
-  console.log(req.body.phone_number);
+
   const userr = await new User({});
   userr.local.phone_number = req.body.phone_number;
   userr.local.password = req.body.password;
   userr.identifier = req.body.phone_number;
+
   User.findOne({ identifier: userr.identifier })
     .then(user => {
       if (!user)
@@ -149,3 +163,58 @@ module.exports.resetPassword = async (req, res, next) => {
       });
     });
 };
+
+
+
+module.exports.updatePassword = (req, res) => {
+
+  const errorResponse = (err) => {
+    return res.status(500).json({
+      success: false,
+      message: 'Error updating password',
+      status: 500,
+      error: {
+        statusCode: 500,
+        message: err.message
+      }
+    });
+  }
+
+  try {
+
+    const { old_password, new_password } = req.body;
+    const identifier = req.user.phone_number;
+
+    User.findOne({ identifier })
+      .then(user => {
+
+        bCrypt.compare(old_password, user.local.password, function(err, result) {
+          if(err) {
+            return errorResponse(err);
+          }
+          if(!result) return errorResponse({message: "Passwords don't match"})
+          bCrypt.hash(new_password, 10, (err, hash) => {
+            user.local.password = hash;
+
+            user.save()
+              .then(result => {
+                res.status(200).json({
+                  success: true,
+                  message: "Password reset successful",
+                  data: {
+                      statusCode: 200,
+                      message: "Password reset successful"
+                  }
+                })
+              })
+              .catch(err => errorResponse(err))
+          });
+        })
+
+      })
+      .catch(err => errorResponse(err))
+  } catch (error) {
+    errorResponse(error);
+  }
+  
+}
--- routes/reset.js
+++ routes/reset.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const Reset = require("../controllers/reset.controller.js");
+const auth = require('../auth/auth');
+const bodyValidator = require('../util/body_validator');
 
 const router = express.Router();
 
 router.post('/recover', Reset.recover);
 
 router.post('/reset', Reset.resetPassword);
+
+router.post('/update/password', auth, Reset.validate('body'), bodyValidator, Reset.updatePassword);
 
 module.exports = router;
