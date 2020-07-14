97850cab45cedd72bcc0fb02526e14abb369bed1
Osondu Tochukwu <tosolife@yahoo.com>
2020-07-14 14:57:02 +0100

    Commit on Statge.

--- models/storeAssistant.js
+++ models/storeAssistant.js
@@ -5,7 +5,7 @@ const storeAssistantSchema = new mongoose.Schema({
 	name: { type: String, required: true},
 	phone_number: { type: String, required: true },
 	email: { type: String, default: "Not set"},
-	password: { type: String, select: false },
+	password: { type: String, default: "password" },
 	store_id: {type: String, required: true},
 	is_active: { type: Boolean, default: 1 },
 	user_role: { type: String, default: "store_assistant" }
